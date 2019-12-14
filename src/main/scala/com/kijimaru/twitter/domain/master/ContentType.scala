d296359fe4c0420714bb61e9fb1e08bdd114325a
kijimaru <eddix.ibanez@outlook.jp>
2019-12-14 17:33:31 +0900

    impl ContentType

---
+++ src/main/scala/com/kijimaru/twitter/domain/master/ContentType.scala
@@ -0,0 +1,17 @@
+package com.kijimaru.twitter.domain.master
+
+case class ContentType(id: Long) {
+  def getValue: Long = id
+}
+
+object ContentType {
+
+  case object Empty extends ContentType(1)
+
+  case object Image extends ContentType(2)
+
+  case object Video extends ContentType(3)
+
+  case object Url extends ContentType(4)
+
+}
